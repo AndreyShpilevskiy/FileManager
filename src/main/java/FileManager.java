d7d2215991a6f235f1ac59f37a9b6a54eb994443
vladimirov <denis.vladimiroff@gmail.com>
2017-03-14 23:22:31 +0200

    Innitial commit

---
+++ src/main/java/FileManager.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
+
+public class FileManager {
+
+    public void createFile(String path, String name) {
+        File file = new File(path + name + ".txt");
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File named \"" + name + "\" created successfully!");
+            } else {
+                System.out.println("File with name \"" + name + "\" already exists!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void deleteFile(String path, String name) {
+        File file = new File(path + name + ".txt");
+        try {
+            if (file.delete()) {
+                System.out.println("File \"" + name + "\" deleted successfully!");
+            } else {
+                System.out.println("Delete operation failed. No such file with name \"" + name + "\".");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void renameFile(String path, String name, String newName) {
+        File file = new File(path + name + ".txt");
+        File newFile = new File(path + newName + ".txt");
+        try {
+            if (file.renameTo(newFile)) {
+                System.out.println("File renamed successful!");
+            } else {
+                System.out.println("File rename operation failed! No such file with name \"" + name + "\" or file with such name already exists.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void findWord(String path, String name, String word) {
+        File file = new File(path + name + ".txt");
+        Charset charset = Charset.forName("US-ASCII");
+        boolean flag = false;
+        try (BufferedReader reader = Files.newBufferedReader(file.toPath(), charset)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(word))
+                    flag = true;
+            }
+            if (flag) {
+                System.out.println("File contains provided word.");
+            } else {
+                System.out.println("Word \"" + word + "\" not found.");
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+    }
+    public void replaceWord(String path, String name, String word, String newWord) {
+        File file = new File(path + name + ".txt");
+        String text = "";
+        String newText;
+        Charset charset = Charset.forName("US-ASCII");
+        try (BufferedReader reader = Files.newBufferedReader(file.toPath(), charset)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (System.getProperty("os.name").contains("Windows")) {
+                    text += line + "\r\n";
+                } else {
+                    text += line + "\n";
+                }
+            }
+            reader.close();
+            if (text.contains(word)) {
+                newText = text.replaceAll(word, newWord);
+                FileWriter writer = new FileWriter(path + name + ".txt");
+                writer.write(newText);
+                writer.close();
+                System.out.println("Word \"" + word + "\" replaced with \"" +newWord+ "\".");
+            } else {
+                System.out.println("Word \"" + word + "\" is missing in the \"" +name+ "\" file.");
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Enter keys to action:");
+        System.out.println("Create File - 1");
+        System.out.println("Delete File - 2");
+        System.out.println("Rename File - 3");
+        System.out.println("Find Word in file - 4");
+        System.out.println("Replace Word in file - 5");
+        System.out.println("Exit - 0");
+
+        do {
+            System.out.println("Enter your command:");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String input = reader.readLine();
+            if (input.equals("0")) {
+                break;
+            }
+            FileManager fileManager = new FileManager();
+            int num = Integer.parseInt(input);
+            System.out.println("Enter path.");
+            //опредделение ОС и предупреждение пользователя о формате, в котором необходимо вводить пути
+            if (isWindows()) {
+                System.out.println("Example: \"C:\\Users\\Desktop\\\"");
+            } else if (isMac()) {
+                System.out.println("Example: \"Macintosh HD:Documents:\"");
+            } else if (isUnix()) {
+                System.out.println("Example: \"/home/user/docs/\"");
+            } else {
+                System.out.println("This is unknown OS");
+            }
+
+            String path = reader.readLine();
+            if (num == 1) {
+                System.out.println("Enter name of new file:");
+                String name = reader.readLine();
+                fileManager.createFile(path, name);
+            } else if (num == 2) {
+                System.out.println("Enter name of file to delete:");
+                String name = reader.readLine();
+                fileManager.deleteFile(path, name);
+            } else if (num == 3) {
+                System.out.println("Enter old name:");
+                String name = reader.readLine();
+                System.out.println("Enter new name:");
+                String newName = reader.readLine();
+                fileManager.renameFile(path, name, newName);
+            } else if (num == 4) {
+                System.out.println("Enter file name:");
+                String name = reader.readLine();
+                System.out.println("Enter word that need to be found:");
+                String word = reader.readLine();
+                fileManager.findWord(path, name, word);
+            } else if (num == 5) {
+                System.out.println("Enter file name:");
+                String name = reader.readLine();
+                System.out.println("Enter word that need to be replaced:");
+                String word = reader.readLine();
+                System.out.println("Enter new word:");
+                String newWord = reader.readLine();
+                fileManager.replaceWord(path, name, word, newWord);
+            } else if (num == 6){
+                System.out.println("Enter name of xls file:");
+            }
+
+        } while (true);
+    }
+    //опредделение ОС и предупреждение пользователя о формате, в котором необходимо вводить пути
+    public static boolean isWindows() {
+        String os = System.getProperty("os.name").toLowerCase();
+        return (os.contains("win"));
+    }
+    public static boolean isMac(){
+        String os = System.getProperty("os.name").toLowerCase();
+        return (os.contains("mac"));
+    }
+    public static boolean isUnix (){
+        String os = System.getProperty("os.name").toLowerCase();
+        return (os.contains("nix") || os.contains("nux"));
+    }
+}
